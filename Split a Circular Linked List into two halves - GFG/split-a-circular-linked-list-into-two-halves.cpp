ad044f30f7eef08f53d3532fdee704a777f9e157
shruti2307y <50659610+shruti2307y@users.noreply.github.com>
2023-10-16 16:17:48 +0530

    Added solution - LeetHub

---
+++ Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.cpp
@@ -0,0 +1,139 @@
+//{ Driver Code Starts
+/* Program to split a circular linked list into two halves */
+
+
+#include<stdio.h> 
+#include<stdlib.h> 
+ 
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+}; 
+
+void splitList(struct Node *head, struct Node **head1_ref,   struct Node **head2_ref);
+ struct Node* newNode(int key)
+{
+    struct Node *temp = new Node(key);
+    
+    return temp;
+}
+/* Function to split a list (starting with head) into two lists.
+   head1_ref and head2_ref are references to head Nodes of 
+    the two resultant linked lists */
+
+void printList(struct Node *head)
+{
+  struct Node *temp = head; 
+  if(head != NULL)
+  {
+    do {
+      printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != head);
+    printf("\n");
+  }
+}
+
+ 
+/* Driver program to test above functions */
+int main()
+{
+  
+   int t,n,i,x;
+    scanf("%d",&t);
+   while(t--)
+   {
+	   struct Node *temp,*head = NULL;
+       struct Node *head1 = NULL;
+       struct Node *head2 = NULL;
+	   scanf("%d",&n);
+	   scanf("%d",&x);
+	   head=new Node(x);
+	   temp=head;
+	   for(i=0;i<n-1;i++){
+	   scanf("%d",&x);
+	   
+	   temp->next=new Node(x);
+	   
+	   temp=temp->next;
+	       
+	   }
+       
+       temp->next=head;
+  
+       splitList(head, &head1, &head2);
+  
+     // printf("\nFirst Circular Linked List");
+      printList(head1);  
+ 
+     // printf("\nSecond Circular Linked List");
+      printList(head2);  
+   
+   }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of linked list is the following
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+};
+*/
+
+//  function which splits the circular linked list.  head is pointer
+// to head Node of given lined list.  head1_ref1 and *head_ref2
+// are pointers to head pointers of resultant two halves.
+
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+    // your code goes here
+    Node *ref=head;
+    Node *curr=head;
+    int c=1;
+    while(curr->next!=ref){
+        c++;
+        curr=curr->next;
+        
+    }
+
+   int mid=c/2+c%2;
+   curr=head;
+   while(mid>1){
+       curr=curr->next;
+       mid--;
+   }
+   Node *temp=curr->next;
+   curr->next=ref;
+   //printf("%d",curr->data);
+   Node *ref2=temp;
+   curr=temp;
+   while(curr->next!=ref){
+       curr=curr->next;
+   }
+   curr->next=ref2;
+   *head2_ref=ref2;
+
+  
+  *head1_ref=head;
+  
+  
+  
+   
+    
+}
